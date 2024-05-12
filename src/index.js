26a99c36acb29749de3e1fd8809157153e5d9bbc
Rishabh Dwivedi <rd14121999@gmail.com>
2024-05-12 15:09:52 +0530

    Added upload video functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
+const multer = require("multer");
 
 const app = express();
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "videos/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "videos", "coke.mp4"));
+});
+
+app.post("/upload", upload.single("video"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
+  res.status(200).send("File uploaded successfully");
 });
 
 const port = process.env.PORT || 3000;
