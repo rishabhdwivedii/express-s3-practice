7dccdee22ae79ca694bc28f59ea0774947216b20
Rishabh Dwivedi <rd14121999@gmail.com>
2024-05-29 21:56:08 +0530

    added feed route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const multer = require("multer");
+const { exec } = require("child_process");
 
 const app = express();
 app.use(cors());
@@ -17,8 +18,29 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "videos", "coke.mp4"));
+app.get("/feed", (req, res) => {
+  const directoryPath = path.join(__dirname, "..", "videos");
+  const command = `ls ${directoryPath}`;
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing command: ${error}`);
+      return res.status(500).send(`Error executing command: ${error.message}`);
+    }
+
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return res.status(500).send(`stderr: ${stderr}`);
+    }
+
+    const files = stdout.split("\n").filter((file) => file);
+    res.json(files);
+  });
+});
+
+app.get("/:id", (req, res) => {
+  const videoName = req.params.id;
+  res.sendFile(path.join(__dirname, "..", "videos", videoName));
 });
 
 app.post("/upload", upload.single("video"), (req, res) => {
